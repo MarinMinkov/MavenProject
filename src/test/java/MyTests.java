b0f309bd43c9eebe2dd76e1fde9936ad5b3743bb
marinminkov <marin.minkov@mentormate.com>
2022-05-15 20:29:27 +0300

    Initial commit

---
+++ src/test/java/MyTests.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+import java.security.PublicKey;
+
+public class MyTests {
+
+    @Test
+    public void SimpleTest()
+    {
+        System.out.println("My first test.");
+    }
+}
