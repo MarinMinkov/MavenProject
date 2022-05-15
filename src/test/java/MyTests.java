675dfc4cf625048b81f042a1df5f465687958935
marinminkov <marin.minkov@mentormate.com>
2022-05-15 21:16:25 +0300

    Added tests

--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -1,6 +1,9 @@
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import java.security.PublicKey;
+import org.testng.asserts.SoftAssert;
 
 public class MyTests {
 
@@ -8,5 +11,73 @@ public class MyTests {
     public void SimpleTest()
     {
         System.out.println("My first test.");
+    }
+    @DataProvider(name = "dataProvider")
+    public Object[][] numbs()
+    {
+        Object[][] objects = {
+                {10, 5},
+                {12, 2},
+        };
+
+
+        return objects;
+    };
+    @Test(groups = "A+B", dataProvider = "dataProvider")
+    public void AB1(int a, int b)
+    {
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(a + b==15);
+        softAssert.assertTrue(a != b);
+        softAssert.assertTrue( a > b);
+        System.out.println(a + b);
+        System.out.println();
+        softAssert.assertAll();
+    }
+    @Test(groups = "A-B", dataProvider = "dataProvider")
+    public void AB2(int a, int b)
+    {
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(a - b==5);
+        softAssert.assertTrue(a != b);
+        softAssert.assertTrue( a > b);
+        System.out.println(a - b);
+        System.out.println();
+        softAssert.assertAll();
+    }
+    @Test(groups = "A*B", dataProvider = "dataProvider")
+    public void AB3(int a, int b)
+    {
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(a * b==50);
+        softAssert.assertTrue(a != b);
+        softAssert.assertTrue( a < b);
+        softAssert.assertTrue( a > b);
+        System.out.println(a * b);
+        System.out.println();
+        softAssert.assertAll();
+    }
+    @Test(groups = "A/B", dataProvider = "dataProvider", dependsOnGroups = "A*B")
+    public void AB4(int a, int b)
+    {
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(a / b==2);
+        softAssert.assertTrue(a != b);
+        softAssert.assertTrue( a < b);
+        softAssert.assertTrue( a > b);
+        System.out.println(a / b);
+        System.out.println();
+        softAssert.assertAll();
+    }
+    @Test(groups = "A%B", dataProvider = "dataProvider", dependsOnGroups = "A*B")
+    public void AB5(int a, int b)
+    {
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(a % b==0);
+        softAssert.assertTrue(a != b);
+        System.out.println(a % b);
+        System.out.println();
+        softAssert.assertAll();
+
     }
 }
